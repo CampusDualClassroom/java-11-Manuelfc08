fd8210850ac8bf6bcd106246eed8f6be4aae9d58
Manuel Fernández Crego <mfernandezc22@fpcoruna.afundacion.org>
2024-09-27 14:28:51 +0200

    Ejer11 Completado.

--- src/main/java/com/campusdual/classroom/Exercise11.java
+++ src/main/java/com/campusdual/classroom/Exercise11.java
@@ -6,6 +6,22 @@ public class Exercise11 {
         TVRemote blackRemote = new TVRemote("Negro");
         System.out.println(redRemote.getColor());
         System.out.println(blackRemote.getColor());
-    }
 
+
+        // subir un canal con el mando rojo
+        redRemote.channelUp();
+        System.out.println("Canal actual después de subir un canal: " + redRemote.channel);
+
+        // Apagar los mandos
+        redRemote.turnOff();
+        blackRemote.turnOff();
+        System.out.println("El mando rojo está encendido después cambiar ? " + redRemote.on);
+        System.out.println("El mando negro está encendido después ? " + blackRemote.on);
+
+        // Subir el volumen dell mando negro
+        blackRemote.turnOn();
+        blackRemote.volumeUp();
+        System.out.println("Volumen actual del mando negro: " + blackRemote.volume);
+
+    }
 }
